b706a3c036225968d92d809135b6e56be967df32
code-schreiber <code-schreiber@users.noreply.github.com>
2018-02-16 15:43:54 +0100

    Trying suff out

--- app/src/main/java/com/toolslab/noisepercolator/view/MainActivity.kt
+++ app/src/main/java/com/toolslab/noisepercolator/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.toolslab.noisepercolator.view
 
 import android.Manifest
+import android.annotation.TargetApi
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -28,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         if (noPermission()) {
             getPermissionToReadSMS()
         } else {
-            refreshSmsInbox();
+            refreshSmsInbox()
         }
     }
 
@@ -38,14 +41,14 @@ class MainActivity : AppCompatActivity() {
                 // If request is cancelled, the result arrays are empty.
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show()
-                    refreshSmsInbox();
+                    refreshSmsInbox()
                 } else {
                     Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show()
                 }
                 return
             }
             else -> {
-                Log.w("MainActivity", "Unknown RequestPermissionsResult: " + requestCode)
+                Log.e("MainActivity", "Unknown RequestPermissionsResult: " + requestCode)
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults)
             }
         }
@@ -62,66 +65,94 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun refreshSmsInbox() {
-        val smsUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Telephony.Sms.CONTENT_URI
-        } else {
-            Uri.parse("content://sms/inbox") // undocumented content provider
-        }
-        val smsInboxCursor = contentResolver.query(smsUri, null, null, null, null)
-        if (smsInboxCursor.moveToFirst()) {
-            Toast.makeText(this, smsInboxCursor.getCount().toString() + " sms", Toast.LENGTH_SHORT).show()
+        val smsUri = getSmsUri()
+        val cursor = contentResolver.query(smsUri, null, null, null, null)
+        if (cursor.moveToFirst()) {
+            var smsAsString = cursor.count.toString() + " sms in " + smsUri + "\n"
 
-            var smsAsString = ""
+
+            val messageId = cursor.getString(cursor.getColumnIndex("_id"))
+            val values = ContentValues()
+            values.put("read", true)
+            val rowsUpdated = contentResolver.update(smsUri, values, "_id" + "=" + messageId, null)
+            smsAsString += rowsUpdated.toString() + " rows updated" + "\n"
+
+            smsAsString += packageName + " is this package" + "\n"
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                Toast.makeText(this, Telephony.Sms.getDefaultSmsPackage(this) + " is DefaultSmsPackage", Toast.LENGTH_SHORT).show()
-
-                val date = smsInboxCursor.getString(smsInboxCursor.getColumnIndexOrThrow(Telephony.Sms.DATE))
-                val address = smsInboxCursor.getString(smsInboxCursor.getColumnIndexOrThrow(Telephony.Sms.ADDRESS))
-                val body = smsInboxCursor.getString(smsInboxCursor.getColumnIndexOrThrow(Telephony.Sms.BODY))
-                val type = smsInboxCursor.getString(smsInboxCursor.getColumnIndexOrThrow(Telephony.Sms.TYPE))
-                val typeDescription =
-                        when ((smsInboxCursor.getString(smsInboxCursor.getColumnIndexOrThrow(Telephony.Sms.TYPE))).toInt()) {
-                            Telephony.Sms.MESSAGE_TYPE_INBOX -> {
-                                Telephony.Sms.MESSAGE_TYPE_INBOX.toString()
-                            }
-                            Telephony.Sms.MESSAGE_TYPE_SENT -> {
-                                Telephony.Sms.MESSAGE_TYPE_SENT.toString()
-                            }
-                            else -> {
-                                TODO("put type in string")
-                                "unknown sms type"
-                            }
-                        }
-                var debugInfo = ""
-                for (i in 0 until smsInboxCursor.columnCount) {
-                    debugInfo += smsInboxCursor.getColumnName(i) + ": " + smsInboxCursor.getString(i) + "; "
-                }
-                do {
-                    smsAsString += address + "\n" +
-                            date + "\n"
-                    body + "\n"
-                    debugInfo + "\n\n"
-                } while (smsInboxCursor.moveToNext())
+                smsAsString += extractSmsAsString(cursor)
             } else {
-                do {
-                    for (i in 0 until smsInboxCursor.columnCount) {
-                        smsAsString += smsInboxCursor.getColumnName(i) + ": " + smsInboxCursor.getString(i) + "; "
-                    }
-                    smsAsString += "\n\n"
-                } while (smsInboxCursor.moveToNext())
+                smsAsString += extractSmsAsStringForDevicesOlderThanKitkat(cursor)
             }
 
-            this.findViewById<TextView>(R.id.activity_main_textdummy).setText(smsAsString)
+            this.findViewById<TextView>(R.id.activity_main_textdummy).text = smsAsString
         } else {
-            Toast.makeText(this, "Empty inbox, no SMS", Toast.LENGTH_SHORT).show()
+            this.findViewById<TextView>(R.id.activity_main_textdummy).text = "Empty inbox, no SMS"
         }
-        smsInboxCursor.close()
+        cursor.close()
     }
 
-    private fun showPermissionExplanation() {
-        Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show()
+    private fun getSmsUri(): Uri? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Telephony.Sms.CONTENT_URI
+        } else {
+            Uri.parse("content://sms/inbox") // undocumented content provider (maybe use Telephony.Sms.CONTENT_URI also?)
+        }
     }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private fun extractSmsAsString(cursor: Cursor): String {
+        var smsAsString = ""
+        smsAsString += Telephony.Sms.getDefaultSmsPackage(this) + " is DefaultSmsPackage" + "\n\n"
+
+        do {
+            val date = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.DATE))
+            val address = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.ADDRESS))
+            val body = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.BODY))
+            val type = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.TYPE))
+            val typeDescription =
+                    when (type.toInt()) {
+                        Telephony.Sms.MESSAGE_TYPE_INBOX -> {
+                            Telephony.Sms.MESSAGE_TYPE_INBOX.toString()
+                        }
+                        Telephony.Sms.MESSAGE_TYPE_SENT -> {
+                            Telephony.Sms.MESSAGE_TYPE_SENT.toString()
+                        }
+                        else -> {
+//                            TODO("put type in string")
+                            "unknown sms type"
+                        }
+                    }
+
+            var debugInfo = ""
+            for (i in 0 until cursor.columnCount) {
+                debugInfo += cursor.getColumnName(i) + ": " + cursor.getString(i) + "; "
+            }
+
+            smsAsString += address + "\n" +
+                    date + "\n" +
+                    body + "\n" +
+                    typeDescription + "\n" +
+                    debugInfo + "\n\n"
+        } while (cursor.moveToNext())
+        return smsAsString
+    }
+
+    private fun extractSmsAsStringForDevicesOlderThanKitkat(cursor: Cursor): String {
+        var smsAsString = ""
+        do {
+            for (i in 0 until cursor.columnCount) {
+                smsAsString += cursor.getColumnName(i) + ": " + cursor.getString(i) + "; "
+            }
+            smsAsString += "\n\n"
+        } while (cursor.moveToNext())
+        return smsAsString
+    }
+
+    private fun showPermissionExplanation() =
+            Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show()
 
     private fun noPermission() =
             ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED
