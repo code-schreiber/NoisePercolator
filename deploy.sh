ef24521f5ff9a7ca901e5032f57f85b883a8bb67
code-schreiber <code-schreiber@users.noreply.github.com>
2018-02-19 20:17:43 +0100

    Add travis files

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+EXPECTED_TRAVIS_REPO_SLUG="code-schreiber/NoisePercolator"
+EXPECTED_TRAVIS_BRANCH="master"
+
+# Script should bail on first error
+set -e
+
+echo "Running deploy.sh"
+echo "deploy.sh: TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "deploy.sh: TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+echo "deploy.sh: TRAVIS_TAG: $TRAVIS_TAG"
+echo "deploy.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+
+echo "deploy.sh: Running gradle build"
+./gradlew build
+#echo "deploy.sh: Running gradle sonarqube"
+#./gradlew sonarqube
+echo "deploy.sh: mobile/build/outputs/apk/release now contains:"
+ls -l mobile/build/outputs/apk/release
+#echo "deploy.sh: Running gradle printStatsFromThisVersion"
+#./gradlew printStatsFromThisVersion
+#echo "deploy.sh: Running gradle printVersion"
+#./gradlew printVersion
+
+if [ "$TRAVIS_REPO_SLUG" != "$EXPECTED_TRAVIS_REPO_SLUG" ]; then
+  echo "deploy.sh: Skipping deployment: wrong repository. Expected '$EXPECTED_TRAVIS_REPO_SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "deploy.sh: Skipping deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$EXPECTED_TRAVIS_BRANCH" ]; then
+  echo "deploy.sh: Skipping deployment: wrong branch. Expected '$EXPECTED_TRAVIS_BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "deploy.sh: Deploying to Google Play"
+#  echo "deploy.sh: Running gradle firebaseUploadReleaseProguardMapping"
+#  ./gradlew firebaseUploadReleaseProguardMapping
+#  cd ..
+#  echo "deploy.sh: Running fastlane supply"
+#  fastlane supply --version
+#  fastlane supply run --json_key dev-console-api-private-key.json --package_name com.toolslab. --apk app/build/outputs/apk/release/mobile-release.apk --track alpha
+#  echo "deploy.sh: Deployed to Google Play"
+  echo "Thank you, come again."
+  exit $?
+fi
+echo "Exiting deploy.sh, skipping deployment"
