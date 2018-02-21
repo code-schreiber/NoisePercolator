89ab7b3e362d7eab461207fe4f0d5e412c65527c
code-schreiber <code-schreiber@users.noreply.github.com>
2018-02-21 09:47:34 +0100

    Add simple test for SmsReceivedNotifier

---
+++ app/src/test/java/com/toolslab/noisepercolator/notification/SmsReceivedNotifierTest.kt
@@ -0,0 +1,30 @@
+package com.toolslab.noisepercolator.notification
+
+import android.telephony.SmsMessage
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Test
+import org.mockito.Mockito.verify
+
+class SmsReceivedNotifierTest {
+
+    private val mockNotifier: Notifier = mock()
+
+    private val mockSmsMessage: SmsMessage = mock()
+
+    private val underTest: SmsReceivedNotifier = SmsReceivedNotifier(mockNotifier)
+
+    @Test
+    fun postNotification() {
+        val expectedNotificationId = 123
+        val expectedTitle = "123"
+        val expectedText = "expected text"
+        whenever(mockSmsMessage.originatingAddress).thenReturn(expectedTitle)
+        whenever(mockSmsMessage.messageBody).thenReturn(expectedText)
+
+        underTest.postNotification(mockSmsMessage)
+
+        verify(mockNotifier).postNotification(expectedNotificationId, expectedTitle, expectedText)
+    }
+
+}
