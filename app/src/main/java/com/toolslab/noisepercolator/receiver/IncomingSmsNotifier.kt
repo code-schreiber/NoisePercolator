5b792187a9f4626c25bc0e9f81a8426c26363105
code-schreiber <code-schreiber@users.noreply.github.com>
2018-02-22 13:10:18 +0100

    Add IncomingSmsNotifier

---
+++ app/src/main/java/com/toolslab/noisepercolator/receiver/IncomingSmsNotifier.kt
@@ -0,0 +1,33 @@
+package com.toolslab.noisepercolator.receiver
+
+import android.telephony.SmsMessage
+import com.toolslab.noisepercolator.db.FilteredOutSmsSaver
+import com.toolslab.noisepercolator.filter.CustomFilter
+import com.toolslab.noisepercolator.notification.SmsReceivedNotifier
+
+class IncomingSmsNotifier(private val customFilter: CustomFilter = CustomFilter(),
+                          private val smsReceivedNotifier: SmsReceivedNotifier = SmsReceivedNotifier(),
+                          private val filteredOutSmsSaver: FilteredOutSmsSaver = FilteredOutSmsSaver()) {
+
+    fun maybeNotify(smsMessages: List<SmsMessage>) {
+        // Show notification for all messages that where not filtered out
+        notifyNonFilteredMessages(smsMessages)
+
+        // Save information about messages that where filtered out
+        saveInfosAboutFilteredOutMessages(smsMessages)
+    }
+
+    private fun notifyNonFilteredMessages(smsMessages: List<SmsMessage>) {
+        smsMessages
+                .filter { customFilter.shouldNotify(it) }
+                .forEach { smsReceivedNotifier.postNotification(it) }
+    }
+
+    private fun saveInfosAboutFilteredOutMessages(smsMessages: List<SmsMessage>) {
+        smsMessages.filter { !customFilter.shouldNotify(it) }
+                .forEach {
+                    filteredOutSmsSaver.saveInfosAbout(it)
+                }
+    }
+
+}
