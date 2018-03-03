564161408efc060b5b2f3c6e17b5e57aaf0a8f08
code-schreiber <code-schreiber@users.noreply.github.com>
2018-03-03 17:24:46 +0100

    Add saving messages to db

--- app/src/main/java/com/toolslab/noisepercolator/db/DataProvider.kt
+++ app/src/main/java/com/toolslab/noisepercolator/db/DataProvider.kt
@@ -2,11 +2,15 @@ package com.toolslab.noisepercolator.db
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.support.annotation.CheckResult
 import android.support.annotation.VisibleForTesting
 import androidx.content.edit
 import com.toolslab.noisepercolator.NoisePercolator
+import com.toolslab.noisepercolator.model.Message
+import com.toolslab.noisepercolator.view.messages.MessageConverter
 
-class DataProvider(private val context: Context = NoisePercolator.applicationContext()) {
+class DataProvider(private val context: Context = NoisePercolator.applicationContext(),
+                   private val messageConverter: MessageConverter = MessageConverter()) {
 
     companion object {
 
@@ -15,8 +19,12 @@ class DataProvider(private val context: Context = NoisePercolator.applicationCon
 
         @VisibleForTesting
         const val NUMBER_OF_MESSAGES_KEY = "NUMBER_OF_MESSAGES_KEY"
+
+        @VisibleForTesting
+        const val MESSAGES_KEY = "MESSAGES_KEY"
     }
 
+    @CheckResult
     fun getNumberOfMessages(): Int {
         return getInt(NUMBER_OF_MESSAGES_KEY, 0)
     }
@@ -24,6 +32,25 @@ class DataProvider(private val context: Context = NoisePercolator.applicationCon
     fun setNumberOfMessages(numberOfMessages: Int) {
         getPreferences().edit {
             putInt(NUMBER_OF_MESSAGES_KEY, numberOfMessages)
+        }
+    }
+
+    @CheckResult
+    fun getMessages(): List<Message> {
+        val messagesAsStringSet = getMessagesStringSet()
+        return messagesAsStringSet.map { messageConverter.convert(it) }
+    }
+
+    @CheckResult
+    fun getMessagesStringSet(): MutableSet<String> {
+        return getPreferences().getStringSet(MESSAGES_KEY, emptySet())
+    }
+
+    fun saveMessage(message: Message) {
+        val messages = getMessagesStringSet()
+        messages.add(messageConverter.convert(message))
+        getPreferences().edit {
+            putStringSet(MESSAGES_KEY, messages)
         }
     }
 
--- app/src/test/java/com/toolslab/noisepercolator/db/DataProviderTest.kt
+++ app/src/test/java/com/toolslab/noisepercolator/db/DataProviderTest.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
+import com.toolslab.noisepercolator.model.Message
+import com.toolslab.noisepercolator.view.messages.MessageConverter
 import org.amshove.kluent.shouldEqual
 import org.junit.Before
 import org.junit.Test
@@ -13,23 +15,29 @@ class DataProviderTest {
 
     companion object {
         private const val A_NUMBER = 123
+        private val MESSAGE = Message("", 0L, "", false, "")
+        private val MESSAGE_AS_STRING = "a message"
+        private val MESSAGES = setOf(MESSAGE_AS_STRING)
     }
 
-    private val mockContext: Context = mock()
     private val mockSharedPreferences: SharedPreferences = mock()
     private val mockEditor: SharedPreferences.Editor = mock()
+    private val mockContext: Context = mock()
+    private val mockMessageConverter: MessageConverter = mock()
 
-    private val underTest = DataProvider(mockContext)
+    private val underTest = DataProvider(mockContext, mockMessageConverter)
 
     @Before
     fun setUp() {
         whenever(mockContext.getSharedPreferences(DataProvider.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)).thenReturn(mockSharedPreferences)
         whenever(mockSharedPreferences.edit()).thenReturn(mockEditor)
+        whenever(mockMessageConverter.convert(MESSAGE)).thenReturn(MESSAGE_AS_STRING)
+        whenever(mockMessageConverter.convert(MESSAGE_AS_STRING)).thenReturn(MESSAGE)
     }
 
     @Test
     fun getNumberOfMessages() {
-        whenever(mockSharedPreferences.getInt(DataProvider.NUMBER_OF_MESSAGES_KEY, 0)).thenReturn(Companion.A_NUMBER)
+        whenever(mockSharedPreferences.getInt(DataProvider.NUMBER_OF_MESSAGES_KEY, 0)).thenReturn(A_NUMBER)
 
         val result = underTest.getNumberOfMessages()
 
@@ -49,6 +57,49 @@ class DataProviderTest {
 
         inOrder(mockEditor).apply {
             verify(mockEditor).putInt(DataProvider.NUMBER_OF_MESSAGES_KEY, A_NUMBER)
+            verify(mockEditor).apply()
+        }
+    }
+
+    @Test
+    fun getMessages() {
+        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(MESSAGES)
+
+        val result = underTest.getMessages()
+
+        result.size shouldEqual 1
+        result[0] shouldEqual MESSAGE
+    }
+
+    @Test
+    fun getMessagesDefault() {
+        val result = underTest.getMessages()
+
+        result shouldEqual emptyList()
+    }
+
+    @Test
+    fun getMessagesStringSet() {
+        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(MESSAGES)
+
+        val result = underTest.getMessagesStringSet()
+
+        result shouldEqual MESSAGES
+    }
+
+    @Test
+    fun getMessagesStringSetDefault() {
+        val result = underTest.getMessagesStringSet()
+
+        result shouldEqual emptySet()
+    }
+
+    @Test
+    fun saveMessage() {
+        underTest.saveMessage(MESSAGE)
+
+        inOrder(mockEditor).apply {
+            verify(mockEditor).putStringSet(DataProvider.MESSAGES_KEY, MESSAGES)
             verify(mockEditor).apply()
         }
     }
