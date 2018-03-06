5c6f6847702c7dab2960bc5f2ddae8ef504420ea
code-schreiber <code-schreiber@users.noreply.github.com>
2018-03-06 11:39:31 +0100

    Less boilerplate test code: Use data class https://kotlinlang.org/docs/reference/data-classes.html

--- app/src/main/java/com/toolslab/noisepercolator/model/Message.kt
+++ app/src/main/java/com/toolslab/noisepercolator/model/Message.kt
@@ -3,11 +3,11 @@ package com.toolslab.noisepercolator.model
 import java.text.DateFormat
 import java.util.*
 
-class Message(internal val address: String,
-              internal val date: Long,
-              internal val body: String,
-              internal val spam: Boolean = false,
-              internal val debugInfo: String) {
+data class Message(internal val address: String,
+                   internal val date: Long,
+                   internal val body: String,
+                   internal val spam: Boolean,
+                   internal val debugInfo: String) {
 
     fun getFormattedDate(): String = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(Date(date))
 
