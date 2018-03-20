c5302fb9cd57906b4ae64ab4d7b80507bf6ffe06
code-schreiber <code-schreiber@users.noreply.github.com>
2018-03-20 15:09:18 +0100

    Run instrumented tests on CI server

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ echo "deploy.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
 echo "deploy.sh: Running gradle build"
 ./gradlew build
-echo "deploy.sh: Running gradle assembleAndroidTest"
-./gradlew assembleAndroidTest
+echo "deploy.sh: Running gradle connectedAndroidTest"
+./gradlew connectedAndroidTest
 #echo "deploy.sh: Running gradle sonarqube"
 #./gradlew sonarqube
 echo "deploy.sh: app/build/outputs/apk/release now contains:"
