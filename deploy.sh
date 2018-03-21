c2f5377f3f2dfd908c4aa1f56e12699ac435c277
code-schreiber <code-schreiber@users.noreply.github.com>
2018-03-21 11:55:23 +0100

    Change call to legacy one

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ echo "deploy.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "deploy.sh: Running gradle build"
 ./gradlew build
 echo "deploy.sh: Creating emulator"
-echo no | android create avd -n emulatorApi26 -k "system-images;android-26;x86"
+echo no | android create avd -n emulatorApi26 -t android-26 --abi armeabi-v7a
 echo "deploy.sh: Starting emulator"
 emulator -avd emulatorApi26 -no-audio -no-window &
 echo "deploy.sh: Waiting for emulator to be ready"
