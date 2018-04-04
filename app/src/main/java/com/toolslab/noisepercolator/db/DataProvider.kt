8a29c2516643d5c18e1d4769852337532fdebb20
code-schreiber <code-schreiber@users.noreply.github.com>
2018-04-04 12:35:45 +0200

    Deprecated shared preferences, now using Realm. Added tests where possible.

--- app/src/main/java/com/toolslab/noisepercolator/db/DataProvider.kt
+++ app/src/main/java/com/toolslab/noisepercolator/db/DataProvider.kt
@@ -8,9 +8,12 @@ import androidx.content.edit
 import com.toolslab.noisepercolator.NoisePercolator
 import com.toolslab.noisepercolator.model.Message
 import com.toolslab.noisepercolator.view.messages.MessageConverter
+import io.reactivex.Observable
+import timber.log.Timber
 
 class DataProvider(private val context: Context = NoisePercolator.applicationContext(),
-                   private val messageConverter: MessageConverter = MessageConverter()) {
+                   private val messageConverter: MessageConverter = MessageConverter(),
+                   private val realmWrapper: RealmWrapper = RealmWrapper()) {
 
     companion object {
 
@@ -19,21 +22,63 @@ class DataProvider(private val context: Context = NoisePercolator.applicationCon
 
         @VisibleForTesting
         const val MESSAGES_KEY = "MESSAGES_KEY"
+
+        const val SHARED_PREFERENCES_DEPRECATED_MESSAGE = "Now using Realm instead of SharedPreferences."
     }
 
     @CheckResult
-    fun getMessages(): List<Message> {
+    fun getMessages(): Observable<List<Message>> {
+        val realm = realmWrapper.getDefaultInstance()
+        migrateOldDatabaseIfNeeded()
+        return realm.where(Message::class.java).findAllAsync()
+                .asFlowable()
+                .toObservable()
+                .map {
+                    realm.copyFromRealm(it)
+                }
+                .doOnDispose {
+                    // Close database when observable is not needed anymore
+                    realm.close()
+                }
+                .doOnError {
+                    // TODO ASK Close database?
+                    Timber.e("Error in DataProvider.getMessages()", it)
+                }
+    }
+
+    fun saveMessages(messages: List<Message>) {
+        messages.forEach { saveMessage(it) }
+    }
+
+    fun saveMessage(message: Message) {
+        // Get a Realm instance for this thread
+        val realm = realmWrapper.getDefaultInstance()
+        // Persist your data in a transaction
+        realm.executeTransaction {
+            // Using executeTransaction with a lambda reduces code size
+            // and makes it impossible to forget to commit the transaction.
+            it.copyToRealm(message)
+            // Close database after saving
+            it.close()
+        }
+    }
+
+    @Deprecated("$SHARED_PREFERENCES_DEPRECATED_MESSAGE Use DataProvider.getMessages().",
+            ReplaceWith("getMessages()", "com.toolslab.noisepercolator.db.DataProvider.getMessages"))
+    fun getMessagesFromSharedPreferences(): List<Message> {
         val messagesAsStringSet = getMessagesStringSet()
         return messagesAsStringSet.map { messageConverter.convert(it) }
     }
 
+    @Deprecated(SHARED_PREFERENCES_DEPRECATED_MESSAGE)
     @VisibleForTesting
-    @CheckResult
     fun getMessagesStringSet(): MutableSet<String> {
         return getPreferences().getStringSet(MESSAGES_KEY, mutableSetOf())
     }
 
-    fun saveMessage(message: Message) {
+    @Deprecated("$SHARED_PREFERENCES_DEPRECATED_MESSAGE Use DataProvider.saveMessage(message).",
+            ReplaceWith("saveMessage(message)", "com.toolslab.noisepercolator.db.DataProvider.saveMessage"))
+    fun saveMessageToSharedPreferences(message: Message) {
         val messages = getMessagesStringSet().toMutableSet()
         messages.add(messageConverter.convert(message))
         getPreferences().edit {
@@ -41,14 +86,24 @@ class DataProvider(private val context: Context = NoisePercolator.applicationCon
         }
     }
 
-    fun clearPreferences() {
+    private fun migrateOldDatabaseIfNeeded() {
+        if (getPreferences().contains(MESSAGES_KEY)) {
+            val messages = getMessagesFromSharedPreferences()
+            Timber.d("Database with {} items will be migrated", messages.size)
+            saveMessages(messages)
+            clearSharedPreferences()
+            Timber.d("Database was migrated")
+        }
+    }
+
+    @Deprecated(SHARED_PREFERENCES_DEPRECATED_MESSAGE)
+    fun clearSharedPreferences() {
         getPreferences().edit {
             clear()
         }
     }
 
-    private fun getInt(key: String, defValue: Int) = getPreferences().getInt(key, defValue)
-
+    @Deprecated(SHARED_PREFERENCES_DEPRECATED_MESSAGE)
     private fun getPreferences(): SharedPreferences {
         return context.getSharedPreferences(SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
     }
--- app/src/test/java/com/toolslab/noisepercolator/db/DataProviderTest.kt
+++ app/src/test/java/com/toolslab/noisepercolator/db/DataProviderTest.kt
@@ -2,29 +2,42 @@ package com.toolslab.noisepercolator.db
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.nhaarman.mockito_kotlin.inOrder
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.*
 import com.toolslab.noisepercolator.model.Message
 import com.toolslab.noisepercolator.view.messages.MessageConverter
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import io.realm.Realm
+import io.realm.RealmQuery
+import io.realm.RealmResults
 import org.amshove.kluent.shouldEqual
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
+
 
 class DataProviderTest {
 
     companion object {
         private const val MESSAGE_AS_STRING = "a message"
-        private val MESSAGE = Message("", 0L, "")
-        private val MESSAGES = setOf(MESSAGE_AS_STRING)
+        private const val ANOTHER_MESSAGE_AS_STRING = "another message"
+        private val MESSAGE = Message("an address", 0L, "a body")
+        private val ANOTHER_MESSAGE = Message("another address", 1L, "another body")
+        private val MESSAGES = mutableListOf(MESSAGE, ANOTHER_MESSAGE)
     }
+
+    private val mockRealmWrapper: RealmWrapper = mock()
+    private val mockRealm: Realm = mock()
+    private val mockRealmQuery: RealmQuery<Message> = mock()
+    private val mockRealmResults: RealmResults<Message> = mock()
 
     private val mockSharedPreferences: SharedPreferences = mock()
     private val mockEditor: SharedPreferences.Editor = mock()
     private val mockContext: Context = mock()
     private val mockMessageConverter: MessageConverter = mock()
 
-    private val underTest = DataProvider(mockContext, mockMessageConverter)
+    private val underTest = DataProvider(mockContext, mockMessageConverter, mockRealmWrapper)
 
     @Before
     fun setUp() {
@@ -32,32 +45,86 @@ class DataProviderTest {
         whenever(mockSharedPreferences.edit()).thenReturn(mockEditor)
         whenever(mockMessageConverter.convert(MESSAGE)).thenReturn(MESSAGE_AS_STRING)
         whenever(mockMessageConverter.convert(MESSAGE_AS_STRING)).thenReturn(MESSAGE)
+        whenever(mockMessageConverter.convert(ANOTHER_MESSAGE)).thenReturn(ANOTHER_MESSAGE_AS_STRING)
+        whenever(mockMessageConverter.convert(ANOTHER_MESSAGE_AS_STRING)).thenReturn(ANOTHER_MESSAGE)
+
+        whenever(mockRealmWrapper.getDefaultInstance()).thenReturn(mockRealm)
+        whenever(mockRealm.copyFromRealm(mockRealmResults)).thenReturn(MESSAGES)
+        whenever(mockRealm.where(Message::class.java)).thenReturn(mockRealmQuery)
+        whenever(mockRealmQuery.findAllAsync()).thenReturn(mockRealmResults)
+        whenever(mockRealmResults.asFlowable()).thenReturn(Flowable.just(mockRealmResults))
     }
 
     @Test
     fun getMessages() {
-        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(MESSAGES)
+        val result = underTest.getMessages()
+        val messages = getResultFromObservable(result)
+
+        messages.size shouldEqual MESSAGES.size
+        messages[0] shouldEqual MESSAGE
+        messages[1] shouldEqual ANOTHER_MESSAGE
+
+        verify(mockRealm).where(Message::class.java)
+        verify(mockRealm).copyFromRealm(mockRealmResults)
+        verifyNoMoreInteractions(mockRealm)
+    }
+
+    @Test
+    fun getMessagesOnDispose() {
+        val observable = underTest.getMessages()
+        val testObserver = TestObserver<List<Message>>()
+        observable.subscribe(testObserver)
+
+        verify(mockRealm).where(Message::class.java)
+        verify(mockRealm).copyFromRealm(mockRealmResults)
+        verifyNoMoreInteractions(mockRealm)
+
+//        testObserver.dispose()
+
+//        verify(mockRealm).close() TODO Real method gets called
+        verifyNoMoreInteractions(mockRealm)
+    }
+
+    @Test
+    fun getMessagesOnError() {
+        val throwableToReturn = Throwable()
 
         val result = underTest.getMessages()
+        val error = getErrorFromObservable(result, throwableToReturn)
 
-        result.size shouldEqual 1
+        verify(mockRealm).where(Message::class.java)
+        verify(mockRealm).copyFromRealm(mockRealmResults)
+        verifyNoMoreInteractions(mockRealm)
+        error shouldEqual throwableToReturn
+    }
+
+    @Test
+    fun getMessagesFromSharedPreferences() {
+        val messages = setOf(MESSAGE_AS_STRING, ANOTHER_MESSAGE_AS_STRING)
+        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(messages)
+
+        val result = underTest.getMessagesFromSharedPreferences()
+
+        result.size shouldEqual messages.size
         result[0] shouldEqual MESSAGE
+        result[1] shouldEqual ANOTHER_MESSAGE
     }
 
     @Test
     fun getMessagesDefault() {
-        val result = underTest.getMessages()
+        val result = underTest.getMessagesFromSharedPreferences()
 
         result shouldEqual emptyList()
     }
 
     @Test
     fun getMessagesStringSet() {
-        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(MESSAGES)
+        val messages = setOf(MESSAGE_AS_STRING, ANOTHER_MESSAGE_AS_STRING)
+        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(messages)
 
         val result = underTest.getMessagesStringSet()
 
-        result shouldEqual MESSAGES
+        result shouldEqual messages
     }
 
     @Test
@@ -67,24 +134,89 @@ class DataProviderTest {
         result shouldEqual emptySet()
     }
 
+    @Ignore("Complete implementation of saveMessage() test first")
+    @Test
+    fun saveMessages() {
+        underTest.saveMessages(MESSAGES)
+    }
+
     @Test
     fun saveMessage() {
+        val captor = argumentCaptor<Realm.Transaction>()
+
         underTest.saveMessage(MESSAGE)
 
+        inOrder(mockRealm).apply {
+            verify(mockRealm).executeTransaction(captor.capture())
+
+//            captor.firstValue.execute(mockRealm) TODO Real method gets called
+
+//            verify(mockRealm).copyToRealm(MESSAGE)
+//            verify(mockRealm).close()
+        }
+    }
+
+    @Test
+    fun saveMessageToSharedPreferences() {
+        underTest.saveMessageToSharedPreferences(MESSAGE)
+
         inOrder(mockEditor).apply {
-            verify(mockEditor).putStringSet(DataProvider.MESSAGES_KEY, MESSAGES)
+            verify(mockEditor).putStringSet(DataProvider.MESSAGES_KEY, setOf(MESSAGE_AS_STRING))
             verify(mockEditor).apply()
         }
     }
 
     @Test
-    fun clearPreferences() {
-        underTest.clearPreferences()
+    fun updateFromSharedPreferencesToRealm() {
+        val originalMessages = setOf(MESSAGE_AS_STRING, ANOTHER_MESSAGE_AS_STRING)
+        whenever(mockSharedPreferences.getStringSet(DataProvider.MESSAGES_KEY, emptySet())).thenReturn(originalMessages)
+        whenever(mockSharedPreferences.contains(DataProvider.MESSAGES_KEY)).thenReturn(true)
+
+        val result = underTest.getMessages()
+        val messages = getResultFromObservable(result)
+
+        // TODO Complete implementation of saveMessage() test first and then verify messages are saved
 
         inOrder(mockEditor).apply {
             verify(mockEditor).clear()
             verify(mockEditor).apply()
         }
+
+        messages.size shouldEqual originalMessages.size
+        messages[0] shouldEqual MESSAGE
+        messages[1] shouldEqual ANOTHER_MESSAGE
+    }
+
+    @Test
+    fun clearPreferences() {
+        underTest.clearSharedPreferences()
+
+        inOrder(mockEditor).apply {
+            verify(mockEditor).clear()
+            verify(mockEditor).apply()
+        }
+    }
+
+    private fun <T> getResultFromObservable(observable: Observable<T>): T {
+        val testObserver = TestObserver<T>()
+        observable.subscribe(testObserver)
+        testObserver.awaitTerminalEvent()
+
+        testObserver.values().size shouldEqual 1
+        testObserver.errors().size shouldEqual 0
+        return testObserver.values()[0]
+    }
+
+    private fun <T> getErrorFromObservable(observable: Observable<T>, throwableToReturn: Throwable): Throwable {
+        val testObserver = TestObserver<T>()
+        observable.subscribe(testObserver)
+        testObserver.awaitTerminalEvent()
+
+        testObserver.onError(throwableToReturn)
+
+        testObserver.values().size shouldEqual 1
+        testObserver.errors().size shouldEqual 1
+        return testObserver.errors()[0]
     }
 
 }
